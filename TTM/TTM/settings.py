79b568ff384fa251506e7fa855ee31d228443422
ma1z0 <rvoronicev18@gmail.com>
2026-05-08 18:07:48 +0200

    Add task filtering search ordering and pagination

--- TTM/TTM/settings.py
+++ TTM/TTM/settings.py
@@ -57,8 +57,11 @@ REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated'
-    ]
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
 }
 
 TEMPLATES = [
--- TTM/tasks/views.py
+++ TTM/tasks/views.py
@@ -1,6 +1,7 @@
 from django.db.models import Q
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import filters
 
 from tasks.models import Task
 from tasks.serializers import TaskSerializer
@@ -9,11 +10,23 @@ from tasks.permissions import IsOwnerOrAssigneeLimited
 class TaskViewSet(viewsets.ModelViewSet):
     serializer_class = TaskSerializer
     permission_classes = [IsAuthenticated, IsOwnerOrAssigneeLimited]
+
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['title', 'description']
+    ordering_fields = ['due_date', 'created_at', 'updated_at', 'completed_at', 'status']
+    ordering = ["-created_at"]
     
-    def get_queryset(self): 
-        return Task.objects.filter(
+    def get_queryset(self):
+
+        queryset = Task.objects.filter(
             Q(owner=self.request.user) | Q(assignee=self.request.user)
         ).distinct()
+
+        task_status = self.request.query_params.get('status')
+        if task_status:
+            queryset = queryset.filter(status=task_status)
+
+        return queryset
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
