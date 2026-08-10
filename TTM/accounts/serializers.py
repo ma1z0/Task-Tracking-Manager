ab679dee083ae0d8c4e9e130b9e40a3fb70103c7
ma1z0 <rvoronicev18@gmail.com>
2026-08-10 12:57:03 +0200

    Add registration validation tests

--- TTM/accounts/serializers.py
+++ TTM/accounts/serializers.py
@@ -1,11 +1,12 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
 
 User = get_user_model()
 
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+    password = serializers.CharField(write_only=True, validators=[validate_password])
 
     class Meta:
         model = User
--- TTM/accounts/tests.py
+++ TTM/accounts/tests.py
@@ -5,7 +5,7 @@ from rest_framework import status
 
 class RegistrationAPITests(APITestCase):
     def test_user_can_register(self):
-        data = {"username": "test", "password": "test"}
+        data = {"username": "test", "password": "fdsme#MfimO#00@93#0#_"}
         response = self.client.post("/api/auth/register/", data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
@@ -14,3 +14,29 @@ class RegistrationAPITests(APITestCase):
         self.assertEqual(created_user.username, data["username"])
         self.assertTrue(created_user.check_password(data["password"]))
         self.assertNotIn("password", response.data)
+
+    def test_registration_rejects_duplicate_username(self):
+        User = get_user_model()
+
+        User.objects.create_user(
+            username="test",
+            password="fdsme#MfimO#00@93#0#_",
+        )
+
+        data = {"username": "test", "password": "fdsme#MfimO#00@93#0#_"}
+
+        response = self.client.post("/api/auth/register/", data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        users_count = User.objects.filter(username="test").count()
+        self.assertEqual(users_count, 1)
+
+    def test_registration_rejects_weak_password(self):
+        User = get_user_model()
+        data = {"username": "ma1z0", "password": "123"}
+
+        response = self.client.post("/api/auth/register/", data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        users_count = User.objects.filter(username="ma1z0").count()
+        self.assertEqual(users_count, 0)
